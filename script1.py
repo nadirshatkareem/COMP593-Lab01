52980d6752cecefe184e9fb6fe85a5c3fe2a8b0c
nadirshatkareem <nadirsha.thannickalkareem@flemingcollege.ca>
2023-05-17 15:47:10 -0400

    Revert "modified files"
    This reverts commit e64d673b55b906dda7c73e015360dfd9bb51afcf.

--- script1.py
+++ script1.py
@@ -1,1 +1,2 @@
 print("this is script file one of the first rep")
+print("this is script file one of the first rep which is modified")
--- script2.py
+++ script2.py
@@ -1,2 +1,1 @@
 print("this is script two of the first rep")
-print("this is script file two of the first rep which is modified now")
